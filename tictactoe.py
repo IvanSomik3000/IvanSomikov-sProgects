95093759d479be3db1ef5aa1bbffee76f5fb2f20
WCS <i.somikov@yandex.ru>
2025-03-19 19:43:05 +0500

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,409 +1,123 @@
 import turtle
 import time
-
-q1 = ''
-q2 = ''
-q3 = ''
-q4 = ''
-q5 = ''
-q6 = ''
-q7 = ''
-q8 = ''
-q9 = ''
-turtle.speed = 0
-turtle.up()
-turtle.goto(-50, 150)
-turtle.down()
-turtle.goto(-50, -150)
-turtle.up()
-turtle.goto(50, 150)
-turtle.down()
-turtle.goto(50, -150)
-turtle.up()
-turtle.goto(-150, 50)
-turtle.down()
-turtle.goto(150, 50)
-turtle.up()
-turtle.goto(-150, -50)
-turtle.down()
-turtle.goto(150, -50)
-turtle.up()
+t = turtle.Pen()
+t.speed(0)
+t.up()
+def draw_board():
+    t.goto(-50, 150)
+    t.down()
+    t.goto(-50, -150)
+    t.up()
+    t.goto(50, 150)
+    t.down()
+    t.goto(50, -150)
+    t.up()
+    t.goto(-150, 50)
+    t.down()
+    t.goto(150, 50)
+    t.up()
+    t.goto(-150, -50)
+    t.down()
+    t.goto(150, -50)
+    t.up()
+def check_win(player):
+    win_conditions = [
+        [q1, q2, q3], [q4, q5, q6], [q7, q8, q9],
+        [q1, q4, q7], [q2, q5, q8], [q3, q6, q9],
+        [q1, q5, q9], [q3, q5, q7]
+    ]
+    for condition in win_conditions:
+        if all(cell == player for cell in condition):
+            return True
+    return False
+def draw_x(x, y):
+    t.goto(x - 40, y + 40)
+    t.down()
+    t.goto(x + 40, y - 40)
+    t.up()
+    t.goto(x + 40, y + 40)
+    t.down()
+    t.goto(x - 40, y - 40)
+    t.up()
+def draw_o(x, y):
+    t.goto(x, y - 40)
+    t.down()
+    t.circle(40)
+    t.up()
+q1 = q2 = q3 = q4 = q5 = q6 = q7 = q8 = q9 = ''
+draw_board()
 while True:
-    XxX = int(input('Ход Х', 'Напишите число поля'))
-    if XxX == 1:
-        if q1 == '':
-            q1 = 'X'
-            turtle.up()
-            turtle.goto(-140, 140)
-            turtle.down()
-            turtle.goto(-60, 60)
-            turtle.up()
-            turtle.goto(-60, 140)
-            turtle.down()
-            turtle.goto(-140, 60)
-            turtle.up()
-            if q1 == 'X' and q2 == 'X' and q3 == 'X' or q4 == 'X' and q5 == 'X' and q6 == 'X' or q7 == 'X' and q8 == 'X' and q9 == 'X':
-                turtle.write('X is winner!!!')
-                time.sleep(1)
-                break
-            elif q1 == 'X' and q4 == 'X' and q7 == 'X' or q2 == 'X' and q5 == 'X' and q8 == 'X' or q3 == 'X' and q6 == 'X' and q9 == 'X':
-                turtle.write('X is winner!!!')
-                time.sleep(1)
-                break
-        else:
-            print('Айайай ты получаешь за нарушение правил пропуск хода!')
-
-            
-    elif XxX == 2:
-        if q2 == '':
-            q2 = 'X'
-            turtle.up()
-            turtle.goto(-40, 140)
-            turtle.down()
-            turtle.goto(40, 60)
-            turtle.up()
-            turtle.goto(40, 140)
-            turtle.down()
-            turtle.goto(-40, 60)
-            turtle.up()
-            if q1 == 'X' and q2 == 'X' and q3 == 'X' or q4 == 'X' and q5 == 'X' and q6 == 'X' or q7 == 'X' and q8 == 'X' and q9 == 'X':
-                turtle.write('X is winner!!!')
-                time.sleep(1)
-                break
-            elif q1 == 'X' and q4 == 'X' and q7 == 'X' or q2 == 'X' and q5 == 'X' and q8 == 'X' or q3 == 'X' and q6 == 'X' and q9 == 'X':
-                turtle.write('X is winner!!!')
-                time.sleep(1)
-                break
-        else:
-            print('Айайай ты получаешь за нарушение правил пропуск хода!')
-
-    elif XxX == 3:
-        if q3 == '':
-            q3 = 'X'
-            turtle.up()
-            turtle.goto(140, 140)
-            turtle.down()
-            turtle.goto(60, 60)
-            turtle.up()
-            turtle.goto(60, 140)
-            turtle.down()
-            turtle.goto(140, 60)
-            turtle.up()
-            if q1 == 'X' and q2 == 'X' and q3 == 'X' or q4 == 'X' and q5 == 'X' and q6 == 'X' or q7 == 'X' and q8 == 'X' and q9 == 'X':
-                turtle.write('X is winner!!!')
-                time.sleep(1)
-                break
-            elif q1 == 'X' and q4 == 'X' and q7 == 'X' or q2 == 'X' and q5 == 'X' and q8 == 'X' or q3 == 'X' and q6 == 'X' and q9 == 'X':
-                turtle.write('X is winner!!!')
-                time.sleep(1)
-                break
-        else:
-            print('Айайай ты получаешь за нарушение правил пропуск хода!')
-    elif XxX == 4:
-            if q4 == '':
-                q4 = 'X'
-                turtle.up()
-                turtle.goto(-140, 40)
-                turtle.down()
-                turtle.goto(-60, -40)
-                turtle.up()
-                turtle.goto(-60, 40)
-                turtle.down()
-                turtle.goto(-140, -40)
-                turtle.up()
-                if q1 == 'X' and q2 == 'X' and q3 == 'X' or q4 == 'X' and q5 == 'X' and q6 == 'X' or q7 == 'X' and q8 == 'X' and q9 == 'X':
-                    turtle.write('X is winner!!!')
-                    time.sleep(1)
-                    break
-                elif q1 == 'X' and q4 == 'X' and q7 == 'X' or q2 == 'X' and q5 == 'X' and q8 == 'X' or q3 == 'X' and q6 == 'X' and q9 == 'X':
-                    turtle.write('X is winner!!!')
-                    time.sleep(1)
-                    break
-            else:
-                print('Айайай ты получаешь за нарушение правил пропуск хода!')
-    elif XxX == 5:
-            if q5 == '':
-                q5 = 'X'
-                turtle.up()
-                turtle.goto(-40, 40)
-                turtle.down()
-                turtle.goto(40, -40)
-                turtle.up()
-                turtle.goto(40, 40)
-                turtle.down()
-                turtle.goto(-40, -40)
-                turtle.up()
-                if q1 == 'X' and q2 == 'X' and q3 == 'X' or q4 == 'X' and q5 == 'X' and q6 == 'X' or q7 == 'X' and q8 == 'X' and q9 == 'X':
-                    turtle.write('X is winner!!!')
-                    time.sleep(1)
-                    break
-                elif q1 == 'X' and q4 == 'X' and q7 == 'X' or q2 == 'X' and q5 == 'X' and q8 == 'X' or q3 == 'X' and q6 == 'X' and q9 == 'X':
-                    turtle.write('X is winner!!!')
-                    time.sleep(1)
-                    break
-            else:
-                print('Айайай ты получаешь за нарушение правил пропуск хода!')
-
-    elif XxX == 6:
-            if q6 == '':
-                q6 = 'X'
-                turtle.up()
-                turtle.goto(140, 40)
-                turtle.down()
-                turtle.goto(60, -40)
-                turtle.up()
-                turtle.goto(60, 40)
-                turtle.down()
-                turtle.goto(140, -40)
-                turtle.up()
-                if q1 == 'X' and q2 == 'X' and q3 == 'X' or q4 == 'X' and q5 == 'X' and q6 == 'X' or q7 == 'X' and q8 == 'X' and q9 == 'X':
-                    turtle.write('X is winner!!!')
-                    time.sleep(1)
-                    break
-                elif q1 == 'X' and q4 == 'X' and q7 == 'X' or q2 == 'X' and q5 == 'X' and q8 == 'X' or q3 == 'X' and q6 == 'X' and q9 == 'X':
-                    turtle.write('X is winner!!!')
-                    time.sleep(1)
-                    break
-            else:
-                print('Айайай ты получаешь за нарушение правил пропуск хода!')
-
-    elif XxX == 7:
-            if q7 == '':
-                q7 = 'X'
-                turtle.up()
-                turtle.goto(-140, -140)
-                turtle.down()
-                turtle.goto(-60, -60)
-                turtle.up()
-                turtle.goto(-140, -60)
-                turtle.down()
-                turtle.goto(-60, -140)
-                turtle.up()
-                if q1 == 'X' and q2 == 'X' and q3 == 'X' or q4 == 'X' and q5 == 'X' and q6 == 'X' or q7 == 'X' and q8 == 'X' and q9 == 'X':
-                    turtle.write('X is winner!!!')
-                    time.sleep(1)
-                    break
-                elif q1 == 'X' and q4 == 'X' and q7 == 'X' or q2 == 'X' and q5 == 'X' and q8 == 'X' or q3 == 'X' and q6 == 'X' and q9 == 'X':
-                    turtle.write('X is winner!!!')
-                    time.sleep(1)
-                    break
-            else:
-                print('Айайай ты получаешь за нарушение правил пропуск хода!')
-
-    elif XxX == 8:
-            if q8 == '':
-                q8 = 'X'
-                turtle.up()
-                turtle.goto(-40, 40)
-                turtle.down()
-                turtle.goto(40, -40)
-                turtle.up()
-                turtle.goto(40, -40)
-                turtle.down()
-                turtle.goto(-40, 40)
-                turtle.up()
-                if q1 == 'X' and q2 == 'X' and q3 == 'X' or q4 == 'X' and q5 == 'X' and q6 == 'X' or q7 == 'X' and q8 == 'X' and q9 == 'X':
-                    turtle.write('X is winner!!!')
-                    time.sleep(1)
-                    break
-                elif q1 == 'X' and q4 == 'X' and q7 == 'X' or q2 == 'X' and q5 == 'X' and q8 == 'X' or q3 == 'X' and q6 == 'X' and q9 == 'X':
-                    turtle.write('X is winner!!!')
-                    time.sleep(1)
-                    break
-            else:
-                print('Айайай ты получаешь за нарушение правил пропуск хода!')
-
-
-    elif XxX == 9:
-            if q9 == '':
-                q9 = 'X'
-                turtle.up()
-                turtle.goto(140, -140)
-                turtle.down()
-                turtle.goto(60, -60)
-                turtle.up()
-                turtle.goto(140, -60)
-                turtle.down()
-                turtle.goto(60, -140)
-                turtle.up()
-                if q1 == 'X' and q2 == 'X' and q3 == 'X' or q4 == 'X' and q5 == 'X' and q6 == 'X' or q7 == 'X' and q8 == 'X' and q9 == 'X':
-                    turtle.write('X is winner!!!')
-                    time.sleep(1)
-                    break
-                elif q1 == 'X' and q4 == 'X' and q7 == 'X' or q2 == 'X' and q5 == 'X' and q8 == 'X' or q3 == 'X' and q6 == 'X' and q9 == 'X':
-                    turtle.write('X is winner!!!')
-                    time.sleep(1)
-                    break
-            else:
-                print('Айайай ты получаешь за нарушение правил пропуск хода!')
-
-
-
-
-
-    OoO = int(input('Ход O', 'Напишите число поля'))
-    if OoO == 1:
-        if q1 == '':
-            q1 = 'O'
-            turtle.goto(-100, 100)
-            turtle.dot(40)
-            turtle.color('white')
-            turtle.dot(38)
-            turtle.color('black')
-            if q1 == 'O' and q2 == 'O' and q3 == 'O' or q4 == 'O' and q5 == 'O' and q6 == 'O' or q7 == 'O' and q8 == 'O' and q9 == 'O':
-                turtle.write('O is winner!!!')
-                time.sleep(1)
-                break
-            elif q1 == 'O' and q4 == 'O' and q7 == 'O' or q2 == 'O' and q5 == 'O' and q8 == 'O' or q3 == 'O' and q6 == 'O' and q9 == 'O':
-                turtle.write('O is winner!!!')
-                time.sleep(1)
-                break
-        else:
-            print('Айайай ты получаешь за нарушение правил пропуск хода!')
-    elif OoO == 2:
-        if q2 == '':
-            q2 = 'O'
-            turtle.goto(0, 100)
-            turtle.dot(40)
-            turtle.color('white')
-            turtle.dot(38)
-            turtle.color('black')
-            if q1 == 'O' and q2 == 'O' and q3 == 'O' or q4 == 'O' and q5 == 'O' and q6 == 'O' or q7 == 'O' and q8 == 'O' and q9 == 'O':
-                t.write('O is winner!!!')
-                time.sleep(1)
-                break
-            elif q1 == 'O' and q4 == 'O' and q7 == 'O' or q2 == 'O' and q5 == 'O' and q8 == 'O' or q3 == 'O' and q6 == 'O' and q9 == 'O':
-                turtle.write('O is winner!!!')
-                time.sleep(1)
-                break
-        else:
-            print('Айайай ты получаешь за нарушение правил пропуск хода!')
-    elif OoO == 3:
-        if q3 == '':
-            q3 = 'O'
-            turtle.goto(100, 100)
-            turtle.dot(40)
-            turtle.color('white')
-            turtle.dot(38)
-            turtle.color('black')
-            if q1 == 'O' and q2 == 'O' and q3 == 'O' or q4 == 'O' and q5 == 'O' and q6 == 'O' or q7 == 'O' and q8 == 'O' and q9 == 'O':
-                turtle.write('O is winner!!!')
-                time.sleep(1)
-                break
-            elif q1 == 'O' and q4 == 'O' and q7 == 'O' or q2 == 'O' and q5 == 'O' and q8 == 'O' or q3 == 'O' and q6 == 'O' and q9 == 'O':
-                turtle.write('O is winner!!!')
-                time.sleep(1)
-                break
-        else:
-            print('Айайай ты получаешь за нарушение правил пропуск хода!')
-    elif OoO == 4:
-        if q4 == '':
-            q4 = 'O'
-            turtle.goto(-100, 0)
-            turtle.dot(40)
-            turtle.color('white')
-            turtle.dot(38)
-            turtle.color('black')
-            if q1 == 'O' and q2 == 'O' and q3 == 'O' or q4 == 'O' and q5 == 'O' and q6 == 'O' or q7 == 'O' and q8 == 'O' and q9 == 'O':
-                turtle.write('O is winner!!!')
-                time.sleep(1)
-                break
-            elif q1 == 'O' and q4 == 'O' and q7 == 'O' or q2 == 'O' and q5 == 'O' and q8 == 'O' or q3 == 'O' and q6 == 'O' and q9 == 'O':
-                turtle.write('O is winner!!!')
-                time.sleep(1)
-                break
-        else:
-            print('Айайай ты получаешь за нарушение правил пропуск хода!')
-    elif OoO == 5:
-        if q5 == '':
-            q5 = 'O'
-            turtle.goto(0, 0)
-            turtle.dot(40)
-            turtle.color('white')
-            turtle.dot(38)
-            turtle.color('black')
-            if q1 == 'O' and q2 == 'O' and q3 == 'O' or q4 == 'O' and q5 == 'O' and q6 == 'O' or q7 == 'O' and q8 == 'O' and q9 == 'O':
-                turtle.write('O is winner!!!')
-                time.sleep(1)
-                break
-            elif q1 == 'O' and q4 == 'O' and q7 == 'O' or q2 == 'O' and q5 == 'O' and q8 == 'O' or q3 == 'O' and q6 == 'O' and q9 == 'O':
-                turtle.write('O is winner!!!')
-                time.sleep(1)
-                break
-        else:
-            print('Айайай ты получаешь за нарушение правил пропуск хода!')
-    elif OoO == 6:
-        if q6 == '':
-            q6 = 'O'
-            turtle.goto(100, 0)
-            turtle.dot(40)
-            turtle.color('white')
-            turtle.dot(38)
-            turtle.color('black')
-            if q1 == 'O' and q2 == 'O' and q3 == 'O' or q4 == 'O' and q5 == 'O' and q6 == 'O' or q7 == 'O' and q8 == 'O' and q9 == 'O':
-                turtle.write('O is winner!!!')
-                time.sleep(1)
-                break
-            elif q1 == 'O' and q4 == 'O' and q7 == 'O' or q2 == 'O' and q5 == 'O' and q8 == 'O' or q3 == 'O' and q6 == 'O' and q9 == 'O':
-                turtle.write('O is winner!!!')
-                time.sleep(1)
-                break
-        else:
-            print('Айайай ты получаешь за нарушение правил пропуск хода!')
-    elif OoO == 7:
-        if q7 == '':
-            q7 = 'O'
-            turtle.goto(-100, -100)
-            turtle.dot(40)
-            turtle.color('white')
-            turtle.dot(38)
-            turtle.color('black')
-            if q1 == 'O' and q2 == 'O' and q3 == 'O' or q4 == 'O' and q5 == 'O' and q6 == 'O' or q7 == 'O' and q8 == 'O' and q9 == 'O':
-                turtle.write('O is winner!!!')
-                time.sleep(1)
-                break
-            elif q1 == 'O' and q4 == 'O' and q7 == 'O' or q2 == 'O' and q5 == 'O' and q8 == 'O' or q3 == 'O' and q6 == 'O' and q9 == 'O':
-                turtle.write('O is winner!!!')
-                time.sleep(1)
-                break
-        else:
-            print('Айайай ты получаешь за нарушение правил пропуск хода!')
-    elif OoO == 8:
-        if q8 == '':
-            q8 = 'O'
-            turtle.goto(0, -100)
-            turtle.dot(40)
-            turtle.color('white')
-            turtle.dot(38)
-            turtle.color('black')
-            if q1 == 'O' and q2 == 'O' and q3 == 'O' or q4 == 'O' and q5 == 'O' and q6 == 'O' or q7 == 'O' and q8 == 'O' and q9 == 'O':
-                turtle.write('O is winner!!!')
-                time.sleep(1)
-                break
-            elif q1 == 'O' and q4 == 'O' and q7 == 'O' or q2 == 'O' and q5 == 'O' and q8 == 'O' or q3 == 'O' and q6 == 'O' and q9 == 'O':
-                turtle.write('O is winner!!!')
-                time.sleep(1)
-                break
-        else:
-            print('Айайай ты получаешь за нарушение правил пропуск хода!')
-    elif OoO == 9:
-        if q9 == '':
-            q9 = 'O'
-            turtle.goto(100, -100)
-            turtle.dot(40)
-            turtle.color('white')
-            turtle.dot(38)
-            turtle.color('black')
-            if q1 == 'O' and q2 == 'O' and q3 == 'O' or q4 == 'O' and q5 == 'O' and q6 == 'O' or q7 == 'O' and q8 == 'O' and q9 == 'O':
-                turtle.write('O is winner!!!')
-                time.sleep(1)
-                break
-            elif q1 == 'O' and q4 == 'O' and q7 == 'O' or q2 == 'O' and q5 == 'O' and q8 == 'O' or q3 == 'O' and q6 == 'O' and q9 == 'O':
-                turtle.write('O is winner!!!')
-                time.sleep(1)
-                break
-        else:
-            print('Айайай ты получаешь за нарушение правил пропуск хода!')
-            
-    
-
+    # Ход игрока X
+    XxX = int(input('Ход X: Введите номер клетки (1-9): '))
+    if XxX == 1 and q1 == '':
+        q1 = 'X'
+        draw_x(-100, 100)
+    elif XxX == 2 and q2 == '':
+        q2 = 'X'
+        draw_x(0, 100)
+    elif XxX == 3 and q3 == '':
+        q3 = 'X'
+        draw_x(100, 100)
+    elif XxX == 4 and q4 == '':
+        q4 = 'X'
+        draw_x(-100, 0)
+    elif XxX == 5 and q5 == '':
+        q5 = 'X'
+        draw_x(0, 0)
+    elif XxX == 6 and q6 == '':
+        q6 = 'X'
+        draw_x(100, 0)
+    elif XxX == 7 and q7 == '':
+        q7 = 'X'
+        draw_x(-100, -100)
+    elif XxX == 8 and q8 == '':
+        q8 = 'X'
+        draw_x(0, -100)
+    elif XxX == 9 and q9 == '':
+        q9 = 'X'
+        draw_x(100, -100)
+    else:
+        print('Ошибка: клетка уже занята или неверный номер. Пропуск хода!')
+        continue
+    if check_win('X'):
+        t.goto(0, -200)
+        t.write('X победил!!!', align="center", font=("Arial", 24, "normal"))
+        time.sleep(1)
+        break
+    OoO = int(input('Ход O: Введите номер клетки (1-9): '))
+    if OoO == 1 and q1 == '':
+        q1 = 'O'
+        draw_o(-100, 100)
+    elif OoO == 2 and q2 == '':
+        q2 = 'O'
+        draw_o(0, 100)
+    elif OoO == 3 and q3 == '':
+        q3 = 'O'
+        draw_o(100, 100)
+    elif OoO == 4 and q4 == '':
+        q4 = 'O'
+        draw_o(-100, 0)
+    elif OoO == 5 and q5 == '':
+        q5 = 'O'
+        draw_o(0, 0)
+    elif OoO == 6 and q6 == '':
+        q6 = 'O'
+        draw_o(100, 0)
+    elif OoO == 7 and q7 == '':
+        q7 = 'O'
+        draw_o(-100, -100)
+    elif OoO == 8 and q8 == '':
+        q8 = 'O'
+        draw_o(0, -100)
+    elif OoO == 9 and q9 == '':
+        q9 = 'O'
+        draw_o(100, -100)
+    else:
+        print('Ошибка: клетка уже занята или неверный номер. Пропуск хода!')
+        continue
+    if check_win('O'):
+        t.goto(0, -200)
+        t.write('O победил!!!', align="center", font=("Arial", 24, "normal"))
+        time.sleep(1)
+        break
+turtle.done()
